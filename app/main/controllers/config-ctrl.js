809383674d501b58b7a87d6ae9e245d12950d794
Julien Barret <ibarretj@gmail.com>
2015-12-02 17:09:22 +0100

    Moved racetrack selection modal to config panel

--- app/main/controllers/config-ctrl.js
+++ app/main/controllers/config-ctrl.js
@@ -5,8 +5,54 @@
 
 'use strict';
 angular.module('main')
-.controller('ConfigCtrl', function ($log) {
+  .controller('ConfigCtrl', function($log, $ionicModal, $ionicPlatform, Config, $window, $scope) {
 
-  $log.log('Hello from your Controller: ConfigCtrl in module main:. This is your controller:', this);
+    $log.log('Hello from your Controller: ConfigCtrl in module main:. This is your controller:', this);
 
-});
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.openRacetracksModal = function() {
+      $scope.modal.show();
+      $ionicPlatform.ready(function() {
+        // Ready functions
+      });
+      //List of racing tracks
+      $scope.themen = Config.ENV.RACETRACKS;
+
+      $scope.updateThemaLocalStorage = function($index) {
+        // Debug: call by index dynamically
+        console.log('klicked index: ' + $index);
+        console.log($scope.themen[$index].name);
+        console.log('current state: ' + $scope.themen[$index].checked);
+        // Actually doing the localStorage: set item to true/false
+        console.log('recent saved state: ' + $window.localStorage[$index]);
+        $window.localStorage.setItem($index, $scope.themen[$index].checked);
+      };
+      $scope.output = $window.localStorage;
+      console.log($scope.output);
+      $scope.getCheck = function($index) {
+        // get the stored toggle (true or false) and
+        // pass it over to the ng-checked in the html
+        return $window.localStorage[$index];
+      };
+    };
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+  });
