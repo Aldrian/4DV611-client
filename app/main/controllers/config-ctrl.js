12e3943576da7196d6a89d7b5576b4b57ad10952
Julien Barret <ibarretj@gmail.com>
2015-12-03 14:04:19 +0100

    Created a localStorage service and refactored the racetrack select accordingly

--- app/main/controllers/config-ctrl.js
+++ app/main/controllers/config-ctrl.js
@@ -5,7 +5,7 @@
 
 'use strict';
 angular.module('main')
-  .controller('ConfigCtrl', function($log, $ionicModal, $ionicPlatform, Config, $window, $scope) {
+  .controller('ConfigCtrl', function($log, $ionicModal, $ionicPlatform, Config, $window, $scope, LocalStorage) {
 
     $log.log('Hello from your Controller: ConfigCtrl in module main:. This is your controller:', this);
 
@@ -17,27 +17,23 @@ angular.module('main')
     });
     $scope.openRacetracksModal = function() {
       $scope.modal.show();
-      $ionicPlatform.ready(function() {
-        // Ready functions
-      });
+
       //List of racing tracks
       $scope.themen = Config.ENV.RACETRACKS;
 
       $scope.updateThemaLocalStorage = function($index) {
         // Debug: call by index dynamically
-        console.log('klicked index: ' + $index);
+        console.log('clicked index: ' + $index);
         console.log($scope.themen[$index].name);
         console.log('current state: ' + $scope.themen[$index].checked);
         // Actually doing the localStorage: set item to true/false
-        console.log('recent saved state: ' + $window.localStorage[$index]);
-        $window.localStorage.setItem($index, $scope.themen[$index].checked);
+        console.log('recent saved state: ' + LocalStorage.getObject('Racetracks'));
+        LocalStorage.setObject('Racetracks', $index, $scope.themen[$index].checked);
       };
-      $scope.output = $window.localStorage;
-      console.log($scope.output);
       $scope.getCheck = function($index) {
         // get the stored toggle (true or false) and
         // pass it over to the ng-checked in the html
-        return $window.localStorage[$index];
+        return LocalStorage.getObject('Racetracks');
       };
     };
     $scope.closeModal = function() {
---
+++ app/main/services/local-storage-serv.js
@@ -0,0 +1,22 @@
+'use strict';
+angular.module('main')
+.service('LocalStorage', function ($log, $window) {
+
+  $log.log('Hello from your Service: LocalStorage in module main');
+
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  };
+
+});
